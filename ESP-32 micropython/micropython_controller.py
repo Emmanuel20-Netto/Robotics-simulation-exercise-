21e69ee87e07daecf142f525dc2a0460ea8e82e0
Simon-Poelma <simonpoelma@gmail.com>
2025-06-04 21:39:06 +0200

    Update micropython_controller.py

--- ESP-32 micropython/micropython_controller.py
+++ ESP-32 micropython/micropython_controller.py
@@ -72,7 +72,7 @@ class SimplePriorityQueue:
 
 def get_valid_neighbors(r, c, rows, cols, grid):
     neighbors = []
-    for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+    for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]: # Right, Left, Down, Up
         nr, nc = r + dr, c + dc
         if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 0:
             neighbors.append(((nr, nc), 1))
