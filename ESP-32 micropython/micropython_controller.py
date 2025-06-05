b69b58aa6eb0ae610c310d7192a53f0a023f684f
Emmanuel20-Netto <tennengnji@gmail.com>
2025-06-05 13:32:21 +0200

    Update micropython_controller.py

--- ESP-32 micropython/micropython_controller.py
+++ ESP-32 micropython/micropython_controller.py
@@ -73,14 +73,6 @@ class SimplePriorityQueue:
     def is_empty(self):
         return len(self._queue) == 0
 
-def get_valid_neighbors(r, c, rows, cols, grid):
-    neighbors = []
-    for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]: # Right, Left, Down, Up
-        nr, nc = r + dr, c + dc
-        if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 0:
-            neighbors.append(((nr, nc), 1))
-    return neighbors
-
 def dijkstra(grid, start_node, end_node):
     rows, cols = len(grid), len(grid[0])
     if not (0 <= start_node[0] < rows and 0 <= start_node[1] < cols and grid[start_node[0]][start_node[1]] == 0):
